141070cbfdfee96048bfe1a5ff6e6dc4cc1128fd
Abde lilah <85647797+0xGLSS@users.noreply.github.com>
2025-06-20 23:20:58 +0100

    Update main.go

--- main.go
+++ main.go
@@ -7,79 +7,145 @@ import (
 	"strings"
 )
 
-var resultStr string
+// big num 666666
+// (up, 6 6)
 
-func getModdedMultiple(input string, endIdx, num int) []string {
-	start := endIdx
-	words := []string{}
-	for start > 0 && len(words) < num {
-		start--
-		if input[start] == ' ' || start == 0 {
-			word := strings.TrimSpace(input[start:endIdx])
-			words = append([]string{word}, words...) // reverse order
-			endIdx = start
+//I was sitting over there    ,and then BAMM !!
+//[I was sitting over there, and then BAMM, !]
+
+/*
+take: "it was the worst of times (pu, 6)"
+return: [it was the worst of times "(up, 6)"]
+*/
+func customFields(str string) []string {
+	str = strings.ReplaceAll(str, "(", " (")
+	str = strings.ReplaceAll(str, ")", ") ")
+
+	slc := strings.Fields(str)
+	result := []string{}
+
+	for i := 0; i < len(slc); i++ {
+		// Ensure there is a next element
+		if i+1 < len(slc) && slc[i][0] == '(' && slc[i+1][len(slc[i+1])-1] == ')' {
+			combined := slc[i] + slc[i+1]
+			result = append(result, combined)
+			i++ // Skip next element
+		} else {
+			result = append(result, slc[i])
 		}
 	}
-	return words
+
+	return result
 }
 
-func parseMods(str string) {
-	mods := []string{"hex", "bin", "up", "low", "cap"}
+// return words indexes (start & end) and they're tag
+func getWordsTag(slc []string) {
+	validTags := []string{"hex", "bin", "up", "low", "cap"}
 
-	for r := 0; r < len(str); r++ {
-		if str[r] != ')' {
+	n := 1
+
+	for i, w := range slc {
+		if w[0] == '(' && w[len(w)-1] == ')' {
+			for _, tag := range validTags {
+				full := w[1:]
+
+				// get tag
+				if strings.HasPrefix(full, tag) {
+
+					// get num
+					comma := strings.Index(full, ",")
+					if comma == (-1) {
+						n = 1
+					} else {
+						s := strings.TrimSpace(full[comma+1 : len(full)-1])
+						n, _ = strconv.Atoi(string(s))
+					}
+
+					// get words indexes
+					start := i - n
+					end := i
+
+					// modify
+					Modify(slc, tag, start, end)
+
+					// remove tags
+					slc[i] = "#"
+				}
+
+			}
+		} else if w[0] == '(' && w[len(w)-1] != ')' || w[0] != '(' && w[len(w)-1] == ')' {
+			return
+		}
+	}
+}
+
+// take tag, start, end
+// from start to end, apply mod
+func Modify(slc []string, tag string, start, end int) {
+
+	if start < 0 {
+		start = 0
+	}
+
+	switch tag {
+	case "up":
+		for i := start; i < end; i++ {
+			slc[i] = strings.ToUpper(slc[i])
+		}
+	case "cap":
+		for i := start; i < end; i++ {
+			slc[i] = strings.Title(slc[i])
+		}
+	case "bin": // should be numbers (10)
+		for i := start; i < end; i++ {
+			bin, _ := strconv.ParseInt(string(slc[i]), 2, 0)
+			slc[i] = strconv.Itoa(int(bin))
+		}
+	case "hex":
+		for i := start; i < end; i++ {
+			hex, _ := strconv.ParseInt(string(slc[i]), 16, 0)
+
+			slc[i] = strconv.Itoa(int(hex))
+		}
+	}
+}
+
+func startWithVowel(s string) bool {
+	switch s[0] {
+	case 'a', 'e', 'i', 'o', 'u', 'h',
+		'A', 'E', 'I', 'O', 'U', 'H':
+		return true
+	}
+	return false
+}
+
+// I was sitting over there    ,and then BAMM !!
+// I was sitting over there, and then BAMM !!
+func FixSymbols(str []string) {
+	for i := 0; i < len(str)-1; i++ {
+		if str[i+1][0] == ',' {
+			str[i+1] = str[i+1][1:]
+			str[i] += ","
+		}
+	}
+	fmt.Println(str)
+}
+
+// final step: remove brackets and change a to an and return output string
+func RemoveTagChangeA(slc []string) string {
+	result := ""
+
+	for i, w := range slc {
+		if (w == "a" || w == "A") && startWithVowel(slc[i+1]) {
+			result += w + "n "
+		} else if w != "#" {
+			result += w + " "
+		} else {
 			continue
 		}
-		l := r
-		for l > 0 && str[l] != '(' {
-			l--
-		}
-
-		tag := str[l+1 : r]  // cap
-		full := str[l : r+1] // (cap)
-		endIdx := l - 1
-		num := 1
-
-		for _, mod := range mods {
-			if strings.HasPrefix(tag, mod) {
-				if commaIdx := strings.Index(tag, ","); commaIdx != -1 {
-					n, _ := strconv.Atoi(strings.TrimSpace(tag[commaIdx+1:]))
-					num = n
-				}
-				resultStr = strings.Replace(resultStr, full+" ", "", 1)
-				words := getModdedMultiple(str, endIdx, num)
-				Modify(words, mod)
-				break
-			}
-		}
 	}
-}
 
-func Modify(words []string, mod string) {
-	str := strings.Join(words, " ")
-
-	// lastIndex
-	fmt.Println(strings.LastIndex(resultStr, str))
-
-	switch mod {
-	case "hex":
-		n, _ := strconv.ParseInt(str, 16, 0)
-		resultStr = strings.Replace(resultStr, str, strconv.Itoa(int(n)), 1)
-	case "bin":
-		n, _ := strconv.ParseInt(str, 2, 0)
-		resultStr = strings.Replace(resultStr, str, strconv.Itoa(int(n)), 1)
-	case "up":
-		resultStr = strings.Replace(resultStr, str, strings.ToUpper(str), 1)
-	case "low":
-		resultStr = strings.Replace(resultStr, str, strings.ToLower(str), 1)
-	case "cap":
-		for _, w := range words {
-			if len(w) > 0 {
-				cap := strings.ToUpper(string(w[0])) + w[1:]
-				resultStr = strings.Replace(resultStr, w, cap, 1)
-			}
-		}
-	}
+	return result
 }
 
 func main() {
@@ -96,10 +162,10 @@ func main() {
 	}
 
 	input := string(data)
-	resultStr = input
+	slc := customFields(input)
+	getWordsTag(slc)
+	RemoveTagChangeA(slc)
 
-	parseMods(input)
-	fmt.Println(resultStr)
-
-	os.WriteFile(args[1], []byte(resultStr), 0644)
+	fmt.Println(input)
+	FixSymbols(slc)
 }
